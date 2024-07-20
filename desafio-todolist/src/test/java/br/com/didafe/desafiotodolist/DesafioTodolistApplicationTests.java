77023376aef9db95f1a96b43670193b7de396dd4
Didafe <dida.fernando@gmail.com>
2024-07-20 18:41:49 -0300

    Add Construtor Todo.java

--- desafio-todolist/src/test/java/br/com/didafe/desafiotodolist/DesafioTodolistApplicationTests.java
+++ desafio-todolist/src/test/java/br/com/didafe/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -3,11 +3,18 @@ package br.com.didafe.desafiotodolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.didafe.desafiotodolist.entity.Todo;
+
 @SpringBootTest
 class DesafioTodolistApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1","desc todo 1", false, 1);
 	}
 
+
+	@Test
+	void testCreateTodoFailure() {
+	}
 }
